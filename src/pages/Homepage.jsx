1efef80c339c079584db7c300445f4525f48fc39
Anushk Jain <anushk2201@gmail.com>
2024-01-23 20:46:40 +0530

    feat: lorem ipsum removed.

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -30,12 +30,12 @@ const Homepage = () => {
           <span className="text-red-600 font-semibold"> Inspiration!"</span>
         </h1>
 
-        <p className="text-center w-1/2 px-12 py-6">
+        {/* <p className="text-center w-1/2 px-12 py-6">
           Lorem ipsum dolor sit amet consectetur adipisicing elit. Obcaecati
           maxime non, corrupti doloremque quos laboriosam veritatis quibusdam
           libero unde veniam, esse neque similique alias officia nihil maiores
           culpa ipsum minima.
-        </p>
+        </p> */}
         {/* <Link to="/ticket"> */}
           <button className="py-3 my-4 text-xl ticket rounded-3xl hover:bg-black  border-red-600 hover: border-2 shadow-red-600 shadow-2xl px-12 bg-red-600">
             Tickets Coming Soon!!!
